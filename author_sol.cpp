ebf51a98a7b417edefbac303c8535100f73342d7
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-07-15 18:09:04 +0800

    Create author_sol.cpp

---
+++ author_sol.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		long long a, b;
+		cin >> a >> b;
+		if (a == b) {
+			// if 'a' is equal to 'b', then the fans will get infinite excitement
+			cout << 0 << " " << 0;
+		} else {
+			// otherwise, compute the difference of 'a' and 'b' (since this will be their GCD)
+			long long g = abs(a - b);
+			// then to find the minimum number of steps neededto make 'a' and 'b' divisible by 'g' is
+			// to compare the minimum value between the remainder of 'a' and 'g' (the number that
+			// is needed to be added to 'a' to make it a multiple of 'g') or the subtracting 'g'
+			// by the value that is needed to be added to 'a' to make it a multiple of 'g' (since
+			// this will reduce 'a' into the multiple of 'g' wherein it is smaller than the 
+			// original value of 'a')
+			long long m = min(a % g, g - a % g);
+			cout << g << " " << m;
+		}
+		cout << '\n';
+	}
+	return 0;
+}
